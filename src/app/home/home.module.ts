b842b4f380209362e591b76b1f0661321faa4983
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-12 13:04:09 +0200

    feat: home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home.component';
 import { AgenciesComponent } from './agencies/agencies.component';
+import { HomeComponent } from './home.component';
 import { TripsComponent } from './trips/trips.component';
 
 @NgModule({

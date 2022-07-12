e3e0e93ea2ed69ff2e546906bd246f98f4a877cb
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-12 12:28:10 +0200

    feat: home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+  public trips = [
+    {
+      id: 'space-y-moon-1',
+      agencyId: 'space-y',
+      agencyTripCode: 'moon',
+      destination: 'The Moon',
+      places: 14,
+      startDate: '2023-01-01',
+      endDate: '2023-02-01',
+      flightPrice: 1200000,
+      stayingNightPrice: 10000,
+      kind: 'WithStay',
+      status: 'Confirmed',
+      extraLuggagePricePerKilo: 2000,
+      premiumFoodPrice: 0,
+    },
+    {
+      id: 'space-y-mars-2',
+      agencyId: 'space-y',
+      agencyTripCode: 'mars',
+      destination: 'Mars',
+      places: 8,
+      startDate: '2024-01-01',
+      endDate: '2024-05-01',
+      flightPrice: 8400000,
+      stayingNightPrice: 10000,
+      kind: 'WithStay',
+      status: 'Waiting',
+      extraLuggagePricePerKilo: 50000,
+      premiumFoodPrice: 0,
+    },
+    {
+      id: 'green-origin-low-orbit-3',
+      agencyId: 'green-origin',
+      agencyTripCode: 'low-orbit',
+      destination: 'Low Orbit',
+      places: 0,
+      startDate: '2022-04-01',
+      endDate: '2022-04-01',
+      flightPrice: 320000,
+      stayingNightPrice: 0,
+      kind: 'TripOnly',
+      status: 'Waiting',
+      extraLuggagePricePerKilo: 0,
+      premiumFoodPrice: 1000,
+    },
+    {
+      id: 'green-origin-iss-4',
+      agencyId: 'green-origin',
+      agencyTripCode: 'iss',
+      destination: 'ISS',
+      places: 6,
+      startDate: '2022-06-01',
+      endDate: '2022-06-01',
+      flightPrice: 800000,
+      stayingNightPrice: 0,
+      kind: 'TripOnly',
+      status: 'Waiting',
+      extraLuggagePricePerKilo: 0,
+      premiumFoodPrice: 1000,
+    },
+  ];
+  public isReloading = false;
+
+  public getTripsCounter() {
+    return this.trips.length;
+  }
+
+  public reload(): void {
+    this.isReloading = true;
+    console.log('♻️ Reloading...');
+  }
+
+  public getClassForStatus(status: string): string {
+    if (status === 'Confirmed') {
+      return 'green';
+    } else {
+      return 'orange';
+    }
+  }
+  public getClassForPlaces(places: number) {
+    if (places === 0) return 'sold-out';
+    if (places < 8) return 'few-places';
+    return '';
+  }
+
+  ngOnInit(): void {}
+}

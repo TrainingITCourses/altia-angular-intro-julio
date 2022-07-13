797adcd22f386fc346f8aa2b9f2b7bcd9e7ae541
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-13 12:48:46 +0200

    feat: basic forms

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  public formGroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(5),
+      ]),
+      message: new FormControl('Contact with me, please.', [
+        Validators.maxLength(10),
+      ]),
+    });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
 
-
 @NgModule({
-  declarations: [
-    ContactComponent
-  ],
-  imports: [
-    CommonModule,
-    ContactRoutingModule
-  ]
+  declarations: [ContactComponent],
+  imports: [CommonModule, ContactRoutingModule, ReactiveFormsModule],
 })
-export class ContactModule { }
+export class ContactModule {}

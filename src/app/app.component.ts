f2548ef28f4a5c6786725557de7b5b12082c0d2f
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-13 09:52:06 +0200

    feat: home routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,3 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
+
+function sumar(a: number, b: number): number {
+  return a + b;
+}
+sumar(2, 3);
+const sumar2 = function (a: number, b: number): number {
+  return a + b;
+};
+sumar2(5, 6);
+
+const sumar3 = (a: number, b: number): number => {
+  return a + b;
+};
+sumar3(7, 8);
+
+const sumar4 = (a: number, b: number): number => a + b;

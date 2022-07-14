ebb6f7d2b971ac479fdd26a8eba3b3785e6fe6db
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-14 10:35:03 +0200

    feat: find agency by param id

--- src/app/agencies/view-agency/view-agency.component.ts
+++ src/app/agencies/view-agency/view-agency.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/core/services/data.service';
 import { UtilService } from 'src/app/core/services/util.service';
 
 @Component({
@@ -8,10 +9,19 @@ import { UtilService } from 'src/app/core/services/util.service';
   styleUrls: ['./view-agency.component.css'],
 })
 export class ViewAgencyComponent implements OnInit {
-  public agencyId: string;
+  // public agencyId: string;
+  public agency: any;
+  constructor(route: ActivatedRoute, util: UtilService, data: DataService) {
+    const agencyId = util.getIdFromRoute(route);
 
-  constructor(route: ActivatedRoute, util: UtilService) {
-    this.agencyId = util.getIdFromRoute(route);
+    // this.agency = data.agencies.find(function byAgencyId(a) {
+    //   return a.id === agencyId;
+    // });
+
+    // const byAgencyId = (agency: any) => agency.id === agencyId;
+    // this.agency = data.agencies.find(byAgencyId);
+
+    this.agency = data.agencies.find((a) => a.id === agencyId);
   }
 
   ngOnInit(): void {}

33169c6fad6d0c3f5559d5620de1867f92483954
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-15 12:51:59 +0200

    feat: rxjs

--- src/app/home/agencies/agencies.component.ts
+++ src/app/home/agencies/agencies.component.ts
@@ -1,12 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+export type AgencyItemType = {
+  name: string;
+  range: string;
+  status: string;
+};
+export type AgenciesViewType = {
+  agenciesCount: number;
+  agenciesList: AgencyItemType[];
+};
+
 @Component({
   selector: 'app-agencies',
   templateUrl: './agencies.component.html',
   styleUrls: ['./agencies.component.css'],
 })
 export class AgenciesComponent implements OnInit {
-  @Input() public agencies: any[] = [];
+  // @Input() public agencies: AgencyType[] = [];
+  @Input() agenciesView!: AgenciesViewType;
 
   ngOnInit(): void {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, map, Observable, tap } from 'rxjs';
 import { AgencyType } from '../core/models/agency.type';
 import { ApiService } from '../core/services/api.service';
 import { DataService } from '../core/services/data.service';
+import {
+  AgenciesViewType,
+  AgencyItemType,
+} from './agencies/agencies.component';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +14,39 @@ import { DataService } from '../core/services/data.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  public agencies: AgencyType[] = [];
+  // public agencies: AgencyType[] = [];
+  // public agencies$: Observable<AgencyType[]>;
+  public agenciesView$: Observable<AgenciesViewType>;
   public isReloadingAgencies = false;
   public isReloadingTrips = false;
 
   constructor(data: DataService, api: ApiService) {
     // this.agencies = data.agencies;
     // api.getAgencies().subscribe((agencies) => (this.agencies = agencies));
-    api.getAgencies().subscribe((agencies) => (this.agencies = agencies));
+    // this.agencies$ = api.getAgencies();
+    this.isReloadingAgencies = true;
+    this.agenciesView$ = api.getAgencies().pipe(
+      tap((agencies: AgencyType[]) => console.log('FROM API:', agencies)),
+      delay(3000),
+      map((agencies: AgencyType[]) => {
+        const agenciesView: AgenciesViewType = {
+          agenciesCount: agencies.length,
+          agenciesList: agencies.map((agency: AgencyType) => {
+            const agencyItem: AgencyItemType = {
+              name: agency.name,
+              range: agency.range || 'Orbital',
+              status: agency.status.toLocaleLowerCase(),
+            };
+            return agencyItem;
+          }),
+        };
+        return agenciesView;
+      }),
+      tap((agenciesView: AgenciesViewType) => {
+        console.log('TO VIEW', agenciesView);
+        this.isReloadingAgencies = false;
+      })
+    );
   }
 
   public onReload(dataName: string) {
